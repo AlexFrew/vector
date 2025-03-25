513ac58243c566592e07165a31aeea028d16e528
Alexander Frew <alexander_frew@keysight.com>
2025-03-25 13:35:46 +0000

    Array Added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Creating an integer array
+int[] intArray = new int[5] { 1, 2, 3, 4, 5 };
+
+// Creating a string array
+string[] stringArray = new string[3] { "Hello", "World", "!" };
+
+// Printing the arrays
+Console.WriteLine("Integer Array: " + string.Join(", ", intArray));
+Console.WriteLine("String Array: " + string.Join(", ", stringArray));
